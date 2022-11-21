495280ab2268718645effef56e7b06cb771b62dc
linj24 <2linj24@gmail.com>
2022-11-21 00:56:28 -0600

    Set default corner to right angle

--- src/heuristic.py
+++ src/heuristic.py
@@ -1,6 +1,125 @@
 #!/usr/bin/env python3
 
-import rospy
+from enum import Enum
 
-from gazebo_msgs.msg import ModelState, ModelStates
-from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
+import numpy as np
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+# Proportional coefficient for linear velocity
+KP_LIN = 0.5
+# Proportional coefficient for angular velocity
+KP_ANG = 0.01
+# Angular velocity for turning corners
+TURN_VEL = 180
+# Conversion from distance to angle
+DIST_TO_ANG = 90
+# Max linear velocity
+LIN_MAX = 0.26
+# Max angular velocity
+ANG_MAX = 1.82
+
+class State(Enum):
+    INIT = 0
+    FORWARD = 1
+    TURN = 2
+    STABILIZE = 3
+
+class Ranges:
+
+
+class CornerHeuristic(object):
+    def __init__(self):
+        self.state = State.INIT
+        rospy.init_node('corner_heuristic')
+        self.cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        self.scan = rospy.Subscriber("/scan", LaserScan, self.check_state)
+        self.bot_vel = Twist()
+        self.corner_side = 0 # -1 for corner on left, 1 for corner on right
+
+        self.state = State.FORWARD
+
+    def detect_ranges(self, data: LaserScan):
+        for i, d in enumerate(data.ranges):
+            
+
+    def move_forward(self, data: LaserScan):
+        new_twist = Twist()
+        new_twist.linear.x = LIN_MAX
+
+    def turn(self, data: LaserScan):
+        closest_distance = np.amin(data.ranges)
+        closest_angle = np.argmin(data.ranges)
+        self.bot_vel.linear.x = KP_LIN * max(closest_distance, THRESHOLD)
+        
+        # if closest wall is in front
+        if (closest_angle < 30 or closest_angle > 330):
+            self.bot_vel.linear.x = 0
+            # if previous wall is on the right
+            if self.wall_side == 1:
+                self.bot_vel.angular.z = KP_ANG * TURN_VEL
+            # if previous wall is on the left
+            else:
+                self.bot_vel.angular.z = KP_ANG * -TURN_VEL
+        # if closest wall is to the left
+        elif (closest_angle < 180):
+            self.bot_vel.angular.z = KP_ANG * (closest_angle - 90 - 
+                                               (DIST_TO_ANG * (THRESHOLD - closest_distance)))
+            self.wall_side = -1
+        # if closest wall is to the right
+        else:
+            self.bot_vel.angular.z = KP_ANG * (closest_angle - 270 +
+                                               (DIST_TO_ANG * (THRESHOLD - closest_distance)))
+            self.wall_side = 1
+
+        self.cmd_vel.publish(self.bot_vel)
+    def stabilize(self, data):
+        pass
+    
+    def turn(self, data):
+        closest_distance = np.amin(data.ranges)
+        closest_angle = np.argmin(data.ranges)
+        self.bot_vel.linear.x = KP_LIN * max(closest_distance, THRESHOLD)
+        
+        # if closest wall is in front
+        if (closest_angle < 30 or closest_angle > 330):
+            self.bot_vel.linear.x = 0
+            # if previous wall is on the right
+            if self.wall_side == 1:
+                self.bot_vel.angular.z = KP_ANG * TURN_VEL
+            # if previous wall is on the left
+            else:
+                self.bot_vel.angular.z = KP_ANG * -TURN_VEL
+        # if closest wall is to the left
+        elif (closest_angle < 180):
+            self.bot_vel.angular.z = KP_ANG * (closest_angle - 90 - 
+                                               (DIST_TO_ANG * (THRESHOLD - closest_distance)))
+            self.wall_side = -1
+        # if closest wall is to the right
+        else:
+            self.bot_vel.angular.z = KP_ANG * (closest_angle - 270 +
+                                               (DIST_TO_ANG * (THRESHOLD - closest_distance)))
+            self.wall_side = 1
+
+        self.cmd_vel.publish(self.bot_vel)
+
+    def check_state(self, data):
+        if self.state == State.INIT:
+            pass
+        else:
+            if self.state == State.FORWARD:
+                self.move_forward(data)
+            elif self.state == State.TURN:
+                self.turn(data)
+            elif self.state == State.STABILIZE:
+                self.stabilize(data)
+            self.cmd_vel.publish(self.bot_vel)
+
+    def run(self):
+        rospy.spin()
+
+
+if __name__ == '__main__':
+    node = CornerHeuristic()
+    node.run()
--- src/reset_world.py
+++ src/reset_world.py
@@ -43,12 +43,12 @@ class ResetWorld(object):
         self.wall_orientation = Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
 
         # reset position and orientation of the robot
-        self.robot_model_name = "robot"
+        self.robot_model_name = "turtlebot3"
         self.robot_reset_position = Point(x=0.0, y=0.0, z=0.0)
         self.robot_reset_orientation = Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
 
         # goal location
-        self.goal_position = Point(x = WALL_LEN_LONG / 2 + WALL_LEN_SHORT / 2, y = 0, z = 0)
+        self.goal_position = Point(x = 7, y = -7, z = 0)
 
         # flag to keep track of the state of when we're resetting the world and when we're not
         # to avoid sending too many duplicate messages
@@ -58,7 +58,7 @@ class ResetWorld(object):
         self.iteration_num = 0
         
         # ROS subscribers
-        rospy.Subscriber("/gazebo/model_states", ModelStates, self.model_states_received)
+        rospy.Subscriber("/gazebo/model_states", ModelStates, self.model_states_received, queue_size=1)
 
         # ROS publishers
         self.model_states_pub = rospy.Publisher("/gazebo/set_model_state", ModelState, queue_size=10)
@@ -91,30 +91,40 @@ class ResetWorld(object):
         l1_o = quaternion_from_euler(0, 0, 0)
         s2_o = quaternion_from_euler(0, 0, angle)
         l2_o = quaternion_from_euler(0, 0, angle)
-        n_0 = quaternion_from_euler(0, 0, angle / 2 + np.pi/2)
+        n_o = quaternion_from_euler(0, 0, angle / 2 + np.pi/2)
 
         motionless_twist = Twist(linear=Vector3(0,0,0), angular=Vector3(0,0,0))
 
-        m_s1 = ModelState(model_name=self.wall_model_names["s1"], pose=Pose(position=Point(s1_x, s1_y, 0), orientation=s1_o), twist=motionless_twist)
-        m_s2 = ModelState(model_name=self.wall_model_names["s2"], pose=Pose(position=Point(s2_x, s2_y, 0), orientation=s2_o), twist=motionless_twist)
-        m_l1 = ModelState(model_name=self.wall_model_names["l1"], pose=Pose(position=Point(l1_x, l1_y, 0), orientation=l1_o), twist=motionless_twist)
-        m_l2 = ModelState(model_name=self.wall_model_names["l2"], pose=Pose(position=Point(l2_x, l2_y, 0), orientation=l2_o), twist=motionless_twist)
+        m_s1 = ModelState(model_name=self.wall_model_names["s1"], pose=Pose(position=Point(s1_x, s1_y, 0), orientation=Quaternion(*s1_o)), twist=motionless_twist)
+        m_s2 = ModelState(model_name=self.wall_model_names["s2"], pose=Pose(position=Point(s2_x, s2_y, 0), orientation=Quaternion(*s2_o)), twist=motionless_twist)
+        m_l1 = ModelState(model_name=self.wall_model_names["l1"], pose=Pose(position=Point(l1_x, l1_y, 0), orientation=Quaternion(*l1_o)), twist=motionless_twist)
+        m_l2 = ModelState(model_name=self.wall_model_names["l2"], pose=Pose(position=Point(l2_x, l2_y, 0), orientation=Quaternion(*l2_o)), twist=motionless_twist)
 
         self.model_states_pub.publish(m_s1)
         self.model_states_pub.publish(m_s2)
         self.model_states_pub.publish(m_l1)
         self.model_states_pub.publish(m_l2)
 
+        if np.abs(angle) < np.pi / 2:
+            m_n = ModelState(model_name=self.wall_model_names["n"], pose=Pose(position=Point(n_x, n_y, 0), orientation=Quaternion(*n_o)), twist=motionless_twist)
+        else:
+            m_n = ModelState(model_name=self.wall_model_names["n"], pose=Pose(position=Point(15, 0, 0), orientation=Quaternion(x=0, y=0, z=0, w=1)), twist=motionless_twist)
+        self.model_states_pub.publish(m_n)
+
+        self.goal_position = Point(x=s2_x + np.cos(np.pi/2 - angle) * PATH_WIDTH / 2, y = s2_y + np.sin(np.pi/2 - angle) * PATH_WIDTH / 2, z=0.0)
+
 
     def at_goal(self, pose: Pose) -> bool:
-        return ((self.goal_position.x - pose.position.x) ** 2 + (self.goal_position.y - pose.position.y) ** 2) <= GOAL_TOLERANCE
+        rospy.loginfo(f"Distance: {np.sqrt(((self.goal_position.x - pose.position.x) ** 2 + (self.goal_position.y - pose.position.y) ** 2))}")
+        return np.sqrt(((self.goal_position.x - pose.position.x) ** 2 + (self.goal_position.y - pose.position.y) ** 2)) <= GOAL_TOLERANCE
 
 
     def model_states_received(self, data: ModelStates):
 
-        robot_idx = data.name.index("robot")
-        if robot_idx is not None:
-            robot_pose = data.pose[robot_idx].position
+        robot_idx = data.name.index("turtlebot3")
+        if robot_idx is not None and data.pose is not None:
+            robot_pose = data.pose[robot_idx]
+            rospy.loginfo(f"At goal: {self.at_goal(robot_pose)}")
 
             # if the robot has reached the goal, send a reward
             if self.at_goal(robot_pose):
@@ -125,7 +135,7 @@ class ResetWorld(object):
                     self.reset_world_in_progress = True
 
                     # create new corner
-                    self.generate_random_corner()
+                    # self.generate_random_corner()
 
                 # reset robot position
                 p = Pose(position=self.robot_reset_position, orientation=self.robot_reset_orientation)
