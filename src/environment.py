a0d35d9e6cfc6a1f4f267c75ed4e07828198986e
Jason Lin <2linj24@gmail.com>
2022-11-25 17:20:27 -0600

    More state calculations

--- src/environment.py
+++ src/environment.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 import numpy as np
+from std_msgs.msg import Header
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
@@ -22,6 +23,7 @@ class State:
     turned_corner: int
     closest_side: bool
     within_dist: bool
+    last_timestamp: float
     last_corner: int
     last_goal: int
 
@@ -36,17 +38,44 @@ class State:
 
 def calculate_state(
     scan: LaserScan,
-    dist: float,
+    dist_threshold: float,
     odom: Odometry,
     corner: tuple[float, float],
     goal: tuple[float, float],
+    last_state: State | None,
 ) -> State:
     state = State()
 
     closest_angle = np.argmin(scan.ranges)
     closest_dist = scan.ranges[closest_angle]
     state.closest_side = bool(closest_angle < 180)
-    state.within_dist = closest_dist < DIST_TO_WALL
+    state.within_dist = closest_dist < dist_threshold
+
+    goal_angle = angle_between(odom, goal)
+    corner_angle = angle_between(odom, corner)
+    state.goal = angle_to_int(goal_angle)
+    state.corner = angle_to_int(corner_angle)
+    state.turned_corner = (last_state and last_state.turned_corner) or (
+        corner_angle < goal_angle
+        if -np.pi / 2 < corner_angle < np.pi / 2
+        else goal_angle < corner_angle
+    )
+
+    cur_timestamp = odom.header.stamp
+
+
+def angle_to_int(angle: float) -> int:
+    degrees = angle / (2 * np.pi) * 360
+    if 80 <= angle < 100:
+        return 0
+    elif 40 <= angle < 80:
+        return 1
+    elif 100 <= angle < 140:
+        return 2
+    elif -90 < angle < 40:
+        return 3
+    else:
+        return 4
 
 
 def angle_between(odom: Odometry, point: tuple[float, float]) -> float:
