88725b52d1fcad92f8ac9d1f7dd2f8757a58644f
linj24 <2linj24@gmail.com>
2022-12-08 02:11:52 -0600

    Add follower launch config

--- src/follow_policy.py
+++ src/follow_policy.py
@@ -8,30 +8,28 @@ import numpy as np
 from std_msgs.msg import UInt8
 from drift_simulator.msg import StateReward
 
-import state
 import learning
 
 class FollowPolicy:
-    def __init__(
-        self, policy_name: str
-    ):
+    def __init__(self):
 
         rospy.init_node("follower")
-        self.policy = np.loadtxt(os.path.join(learning.CHECKPOINT_DIR, policy_name, "policy.csv"), dtype=int)
+        model_name = rospy.get_param('~model', "heuristic")
+        rospy.loginfo(f"Loading model {model_name}...")
+        self.policy = np.loadtxt(os.path.join(learning.CHECKPOINT_DIR, model_name, "policy.csv"), dtype=int)
         self.action_pub = rospy.Publisher("/action", UInt8, queue_size=10)
         self.state_reward_sub = rospy.Subscriber(
             "/state_reward", StateReward, self.process_SR
         )
 
     def process_SR(self, data: StateReward):
-        r, s, t = data.reward, data.state, data.terminal
+        _, s, _ = data.reward, data.state, data.terminal
         a = self.policy[s]
-        print(a)
         self.action_pub.publish(a)
 
     def run(self):
         rospy.spin()
 
 if __name__ == "__main__":
-    node = FollowPolicy("sarsa_1000")
+    node = FollowPolicy()
     node.run()
